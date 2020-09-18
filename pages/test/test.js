249c50bdd2bd4f9e16739485b13e0218fe44b842
Yang <651033755@qq.com>
2020-09-18 21:17:45 +0800

    全局button组件

--- pages/test/test.js
+++ pages/test/test.js
@@ -18,12 +18,30 @@ Page({
   }
 ],
 propValue:1,
-str:"我是一条信息"
+str:"我是一条信息",
+arr:[1,2,3,4],
+obj:{id:1,text:"我是信息1"},
+xx:false
   },
 
   
   onLoad: function (options) {
 
+  },
+  changeColor(){
+   this.setData({xx: !this.data.xx})
+  },
+  changeObjText(){
+  //  this.obj.text="我是信息2"
+  //  this.setData({obj:this.obj})   这么做不行，不能修改text
+  this.setData({"obj.text":"我是信息2"})  //想要改变某个key里面的值就得这样写
+  },
+  pushItem(){
+    //小程序基本不能用push方法,但下面方法相当于push
+    let last = this.data.arr[this.data.arr.length-1]+1
+    let newArr = [last]
+    this.data.arr = this.data.arr.concat(newArr)
+    this.setData({arr: this.data.arr})
   },
   reverseStr(){
     this.setData({str: this.data.str.split('').reverse().join('')})
