fccc3c1bd2ee1c28fdc92c7233c0fd453ce671f1
Yang <651033755@qq.com>
2020-09-22 20:20:02 +0800

    添加.gitignore

--- app.js
+++
@@ -1,9 +0,0 @@
-App({
-  globalData: {
-      host: 'https://gp-server.hunger-valley.com', 
-      app_id: "wxaf903364ed364a4b",
-      app_secret: "c6f7b5f474fc4731adfc9897eccaea42",
-      t_app_id: "nwRghXm2gRHeZbMwBc5WiGHx",
-      t_app_secret: "WMbH3CA5u8X2J3qvJ8XBqZLW"
-  }
- })
